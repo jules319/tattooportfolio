9f48cbb7309326ee77e5aa48e41827af2dd292dd
jules319 <jules31901@gmail.com>
2023-05-08 21:14:15 -0400

    Create API endpoints of models

--- backend/portfolio/views.py
+++ backend/portfolio/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ArtistSerializer, AboutSerializer, TattooSerializer, TestimonialSerializer
+from .models import Artist, About, Tattoo, Testimonial
 
-# Create your views here.
+class ArtistView(viewsets.ModelViewSet):
+    serializer_class = ArtistSerializer
+    queryset = Artist.objects.all()
+
+class AboutView(viewsets.ModelViewSet):
+    serializer_class = AboutSerializer
+    queryset = About.objects.all()
+
+class TattooView(viewsets.ModelViewSet):
+    serializer_class = TattooSerializer
+    queryset = Tattoo.objects.all()
+
+class TestimonialView(viewsets.ModelViewSet):
+    serializer_class = TestimonialSerializer
+    queryset = Testimonial.objects.all()
