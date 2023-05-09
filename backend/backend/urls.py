2d65cd286c194c47e77da3dccadf0338119c8220
jules319 <jules31901@gmail.com>
2023-05-08 21:16:18 -0400

    specify url paths for api in backend urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from portfolio import views
+
+router = routers.DefaultRouter()
+router.register(r'artists', views.ArtistView, 'artist')
+router.register(r'about', views.AboutView, 'about')
+router.register(r'tattoos', views.TattooView, 'tattoo')
+router.register(r'testimonials', views.TestimonialView, 'testimonial')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
 ]
