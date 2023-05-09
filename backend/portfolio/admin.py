1fadf560c985790dd8359a162f1b1733c01ae859
jules319 <jules31901@gmail.com>
2023-05-08 20:55:06 -0400

    Created initial models and migrated and registered to admin

--- backend/portfolio/admin.py
+++ backend/portfolio/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Artist, Tattoo, Testimonial
 
-# Register your models here.
+admin.site.register(Artist)
+admin.site.register(Tattoo)
+admin.site.register(Testimonial)
--- backend/portfolio/models.py
+++ backend/portfolio/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+    contact_email = models.EmailField()
+    contact_phone = models.CharField(max_length=20, blank=True, null=True)
+    profile_image = models.ImageField(upload_to='artists/', blank=True, null=True)
+
+
+class About(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    profile_image = models.ImageField(upload_to='about/', blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = "About"
+
+
+class Tattoo(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='tattoos/')
+    created_date = models.DateField()
+
+class Testimonial(models.Model):
+    name = models.CharField(max_length=100)
+    text = models.TextField()
+    profile_image = models.ImageField(upload_to='testimonials/', blank=True, null=True)
